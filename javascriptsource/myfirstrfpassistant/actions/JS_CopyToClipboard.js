218aa54de7ce9280f29ce5077ab3eb78a786600e
Tina Engel <tina.engel@mendix.com>
2024-11-04 09:56:09 +0000

    SAS-798 updated copy to clipboard JS action with new navigator API

--- javascriptsource/myfirstrfpassistant/actions/JS_CopyToClipboard.js
+++ javascriptsource/myfirstrfpassistant/actions/JS_CopyToClipboard.js
@@ -17,14 +17,6 @@ import { Big } from "big.js";
  */
 export async function JS_CopyToClipboard(textToCopy) {
 	// BEGIN USER CODE
-const el = document.createElement("textarea");
-    el.value = textToCopy;
-    el.setAttribute("readonly", "");
-    el.style.position = "absolute";
-    el.style.left = "-9999px";
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand("copy");
-    document.body.removeChild(el);
+    navigator.clipboard.writeText(textToCopy);
 	// END USER CODE
 }
